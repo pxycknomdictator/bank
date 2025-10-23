eca9c1a46db079e46d53af66d965c091e3f696c3
Noman <pxycknomdictator@outlook.com>
2025-10-23 22:19:45 +0500

    feat: add external api response interface

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,3 +1,4 @@
 export * from "./models";
 export * from "./tokens";
 export * from "./user.dto";
+export * from "./ip";
---
+++ src/types/ip.d.ts
@@ -0,0 +1,6 @@
+export interface ipLookupResponse {
+	ip: string;
+	country: string;
+	region: string;
+	city: string;
+}
